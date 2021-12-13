5278bb09f188f7692f4b3dac9aebc027c20951e2
kacperora <34722030+kacperora@users.noreply.github.com>
2021-12-13 18:26:13 +0100

    test

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # This is a sample Python script.
 
-from bs4 import BeautifulSoup
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from selenium import webdriver
+
 from urllib.request import urlopen
+import html
 import re
 import random
 
@@ -34,29 +32,51 @@ class Weapon:
             return self.d
         else:
             a = map(int, re.findall(r'\d+', self.d))
-            if
-            sum = a[0]*random.randint(1,a[1])
+            #if
+            #sum = a[0]*random.randint(1,a[1])
 
 
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def loadUnitTable(table):
+    output = []
+    table = html.unescape(table)
+
+    index1 = table.find('">')+2
+    index2 = table.find('<', index1)
+
+    indexname1 = table.find("</div>",table.find("PriceTag"))+6
+    indexname2 = table.find("</span>", indexname1)
+    output.append(table[indexname1:indexname2])
+    while index1 != -1:
+        if table[index1:index2] != '':
+            output.append(table[index1:index2])
+        index1 = table.find('">', index2)
+        if index1 != -1:
+            index1 += 2
+        index2 = table.find('<', index1)
+    return output
+
+def loadUnit(array):
+
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    seed(0)
+    #seed(0)
     page = urlopen("https://wahapedia.ru/wh40k9ed/factions/astra-cartographica/Voidsmen-at-arms")
     html_bytes = page.read()
-    html = html_bytes.decode("utf-8")
-    print(html)
-    title_index = html.find('<tr class="pTable2_short">')
-    title_index = html.find('<tr>')
+    code = html_bytes.decode("utf-8")
+    #print(code)
+    title_index = code.find('<tr class="pTable2_short">')
+    title_index = code.find('<tr>')
     start_index = title_index + len('<tr>')
-    end_index = html.find('</tr>',start_index)
-    table = html[start_index:end_index]
+    end_index = code.find('</tr>',start_index)
+    table = code[start_index:end_index]
     print (table)
+    table = loadUnitTable(table)
+    print(table)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
